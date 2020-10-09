1045215bafe7363577cfaebe321c0f08b59a5b35
周尚仑 <zsl@sz-xunjie.com>
2020-10-09 09:22:36 +0800

    fixbug

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -3,6 +3,7 @@ import { DB as DBConfig, System as SystemConfig } from '../config'
 
 export default new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password, {
   host: DBConfig.host,
+  port: DBConfig.port,
   dialect: SystemConfig.db_type,
   pool: {
     max: 50,
